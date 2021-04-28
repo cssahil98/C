556e7d2ef84392a7583049e49b25af9d576ad8b5
Sahil <83275251+csssahil98@users.noreply.github.com>
2021-04-28 11:52:01 +0530

    Create 2D-Array.c

---
+++ 2D-Array.c
@@ -0,0 +1,33 @@
+/* C Program - Two Dimensional Array */
+		
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int arr[10][10], row, col, i, j;
+	printf("Enter number of row for Array (max 10) : ");
+	scanf("%d",&row);
+	printf("Enter number of column for Array (max 10) : ");
+	scanf("%d",&col);
+	printf("Now Enter %d*%d Array Elements : ",row, col);
+	for(i=0; i<row; i++)
+	{
+		for(j=0; j<col; j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	printf("The Array is :\n");
+	for(i=0; i<row; i++)
+	{
+		for(j=0; j<col; j++)
+		{
+			printf("%d  ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	getch();
+}
+
+
