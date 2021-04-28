d9ce1754aa7a8b5117fae31bdcafe653685928a2
Sahil <83275251+csssahil98@users.noreply.github.com>
2021-04-28 13:26:16 +0530

    swap 2 numbers

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int x, y, t;
+ 
+  printf("Enter two integers\n");
+  scanf("%d%d", &x, &y);
+ 
+  printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+ 
+  t = x;
+  x = y;
+  y = t;
+ 
+  printf("After Swapping\nx = %d\ny = %d\n", x, y);
+ 
+  return 0;
+}
+
+
